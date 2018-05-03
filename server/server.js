880e0301ed1249537835f870ab94ec9cbb24b38f
Rafalsky <rafbe@ymail.com>
2018-05-03 12:40:37 +0200

    connect and disconnect events added

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const websockets = require('socket.io')
 
 const { env } = require('../config/config')
+
 const app = express()
+const server = http.createServer(app)
+const io = websockets(server)
+
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-// app.get('/', (req, res) => {
-  
-// })
+io.on('connection', (socket) => {
+  console.log('client connected:')
 
-app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT} in ${env} mode...`))
+  socket.on('disconnect', () => {
+    console.log('Client disconnected...')
+  })
+})
+
+server.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT} in ${env} mode...`))
