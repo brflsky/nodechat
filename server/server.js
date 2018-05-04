5538fbd9c9dd191bfa391df0a010c1b181ec273b
Rafalsky <rafbe@ymail.com>
2018-05-04 02:56:53 +0200

    add mustache.js for template

--- server/server.js
+++ server/server.js
@@ -1,42 +1,39 @@
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const websockets = require('socket.io')
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
-const { env } = require('../config/config')
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
-const app = express()
-const server = http.createServer(app)
-const io = websockets(server)
-
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('Client connected:')
+  console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to chat...',
-    createAt: new Date().valueOf()
-  })
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New user entered chat',
-    createAt: new Date().valueOf()
-  })
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', data => {
-    console.log('createMessage:', data)
-    io.emit('newMessage', {
-      ...data,
-      createAt: new Date().valueOf()
-    })
-  })
+  socket.on('createMessage', (message, callback) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback();
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
 
   socket.on('disconnect', () => {
-    console.log('Client disconnected...')
-  })
-})
+    console.log('User was disconnected');
+  });
+});
 
-server.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT} in ${env} mode...`))
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
